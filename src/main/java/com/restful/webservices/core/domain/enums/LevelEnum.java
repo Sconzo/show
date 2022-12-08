40e0e98161031f0d341dd567080d956d6483d24b
Sconzo <rspolydoro@gmail.com>
2022-12-08 13:15:15 -0300

    Integration with front for question POST

--- src/main/java/com/restful/webservices/core/domain/enums/LevelEnum.java
+++ src/main/java/com/restful/webservices/core/domain/enums/LevelEnum.java
@@ -2,7 +2,7 @@ package com.restful.webservices.core.domain.enums;
 
 public enum LevelEnum {
     EASY(0,"EASY"),
-    MEDIUM(1,"MEDIUM"),
+    INTERMEDIATE(1,"INTERMEDIATE"),
     HARD(2,"HARD");
 
     private int key;
