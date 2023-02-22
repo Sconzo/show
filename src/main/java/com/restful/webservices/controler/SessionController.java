f98d4225f10b9a960eeb9a5e2919b8649f0440d5
Sconzo <rspolydoro@gmail.com>
2023-02-22 20:46:26 -0300

    [

---
+++ src/main/java/com/restful/webservices/controler/SessionController.java
@@ -0,0 +1,60 @@
+package com.restful.webservices.controler;
+
+import com.restful.webservices.core.domain.dtos.session.SessionRequest;
+import com.restful.webservices.core.domain.dtos.session.SessionResponse;
+import com.restful.webservices.core.services.SessionService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/session")
+public class SessionController {
+
+    private final SessionService sessionService;
+
+    public SessionController(SessionService sessionService) {
+        this.sessionService = sessionService;
+    }
+
+    @GetMapping(path = "")
+    public SessionResponse getSession(
+            @RequestParam Long id,
+            HttpServletResponse servletResponse) {
+
+        SessionResponse sessionResponse = sessionService.getSession(id);
+
+        return sessionResponse;
+    }
+
+    @PostMapping(path = "")
+    public SessionResponse createSession(
+            @RequestBody @Valid SessionRequest sessionRequest,
+            HttpServletResponse servletResponse) {
+
+        SessionResponse sessionResponse = sessionService.createSession(sessionRequest);
+
+        return sessionResponse;
+    }
+
+    @GetMapping(path = "/all")
+    public List<SessionResponse> getAllSessions(
+            HttpServletResponse servletResponse) {
+
+        List<SessionResponse> sessionResponse = sessionService.getAllSession();
+
+        return sessionResponse;
+    }
+
+    @GetMapping(path = "/questions-created/{sessionId}")
+    public Long getNumberQuestionsCreated(
+            @PathVariable Long sessionId,
+            HttpServletResponse servletResponse) {
+
+        Long sessionResponse = sessionService.getNumberOfQuestionsCreated(sessionId);
+
+        return sessionResponse;
+    }
+}
